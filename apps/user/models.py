9c985c7444d063f649f66432a0a6d9d3ee62c0bf
Keeps-on <1299793997@qq.com>
2020-03-09 22:39:57 +0800

    创建了角色权限

--- apps/user/models.py
+++ apps/user/models.py
@@ -14,3 +14,25 @@ class UserProfile(models.Model):
 
     class Meta:
         db_table = "UserProfile"
+
+
+class Role(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='主键自增')
+    name = models.CharField(max_length=12, verbose_name="角色名称")
+
+    def __str__(self):
+        return "<{}>".format(self.name)
+
+    class Meta:
+        db_table = "Role"
+
+
+class Permission(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='主键自增')
+    name = models.CharField(max_length=12, verbose_name="权限名称")
+
+    def __str__(self):
+        return "<{}>".format(self.name)
+
+    class Meta:
+        db_table = "Permission"
