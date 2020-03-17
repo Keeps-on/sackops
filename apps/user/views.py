8ee7dac03f5c84b87770ddfd5fe4934fb5efc02c
Keeps-on <1299793997@qq.com>
2020-03-17 20:48:31 +0800

    增加了创建用户的接口

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render, render_to_response
 
 from django.http import JsonResponse, HttpResponse
+
+
+from user.models import UserProfile
 
 
 # Create your views here.
@@ -13,3 +16,18 @@ def user_list(request):
     :return:
     """
     return render_to_response(template_name='user/user_list.html')
+
+
+def user_create(request):
+    """
+    添加用户
+    :param request:
+    :return:
+    """
+    result = {"code": 0, "msg": ""}
+    # 获取用户信息
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+    # 存入数据
+    UserProfile.objects.create(username=username,password=password)
+    return JsonResponse(result)
