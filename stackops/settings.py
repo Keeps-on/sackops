922a110145c99c7559a2157d52b6f29bcae4dd6f
Keeps-on <1299793997@qq.com>
2020-03-20 11:28:07 +0800

    邮件配置文件

--- stackops/settings.py
+++ stackops/settings.py
@@ -137,3 +137,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+# https://www.cnblogs.com/zyj-python/p/7522471.html
+########### EmailConfig ################
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False   #是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性。)
+EMAIL_USE_SSL = True    #是否使用SSL加密，qq企业邮箱要求使用
+EMAIL_HOST = 'smtp.163.com'   #发送邮件的邮箱 的 SMTP服务器，这里用了163邮箱
+EMAIL_PORT = 465     #发件箱的SMTP服务器端口
+EMAIL_HOST_USER = 'lippjobemail@163.com'    #发送邮件的邮箱地址
+EMAIL_HOST_PASSWORD = 'lipanpan0613'         #发送邮件的邮箱密码(这里使用的是授权码)
+EMAIL_FROM = 'Ops<lippjobemail@163.com>'
+
+########### EmailConfig ################
